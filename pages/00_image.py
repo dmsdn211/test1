a9d2131bf99da31276a84822fc0c97a18a15469f
dmsdn211 <eunwoo468@gmail.com>
2025-06-05 15:44:16 +0900

    Create 00_image.py

---
+++ pages/00_image.py
@@ -0,0 +1,2 @@
+import streamlit as st
+st.image("https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExdXhhYW5wNG1jcGpxejg4bWZ3c21vbjdhZnJnYXB4NGUzczA3aXB3cyZlcD12MV9naWZzX3NlYXJjaCZjdD1n/y4Hl0rKWwVTpu/giphy.gif")
